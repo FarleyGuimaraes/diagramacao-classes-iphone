d9ec0a8f25c01bb6ebf9b48222eb6164ea23c78f
Farley Guimarães <farley.guimaraes@outlook.com.br>
2023-09-01 03:10:10 -0300

    device iPhone

---
+++ src/iphone/devices/Iphone.java
@@ -0,0 +1,33 @@
+package src.iphone.Devices;
+
+import src.iphone.apps.Ipod;
+import src.iphone.apps.Phone;
+import src.iphone.apps.Safari;
+
+public class Iphone{
+    
+    private Phone phone;
+    private Safari safari;
+    private Ipod ipod;
+
+    public Iphone(){
+        this.phone = new Phone();
+        this.safari = new Safari();
+        this.ipod = new Ipod();
+    }
+
+    public Phone phone() {
+        return phone;
+    }
+
+    public Safari safari() {
+        return safari;
+    }
+
+    public Ipod ipod() {
+        return ipod;
+    }
+    
+    
+   
+}
